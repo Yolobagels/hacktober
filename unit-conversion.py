41ef3190e12c1fbadb0ba42a040453a4b04c5e4d
keithtqs <32459806+keithtqs@users.noreply.github.com>
2021-10-31 17:30:48 +0800

    Update unit-conversion.py

--- unit-conversion.py
+++ unit-conversion.py
@@ -3,12 +3,10 @@
 lbs_to_kg = 0.453592
 
 # User input
-option = eval(input("Enter 1 to convert from pounds to kilograms, or 2 to convert from kilograms to pounds: "))
-if option == 1: 
-    pounds = eval(input("Enter a number of pounds: "))
-    kilograms = pounds * lbs_to_kg
-    print("The number of kilograms is", kilograms)
-elif option == 2:
-    kilograms = eval(input("Enter a number of kilograms: "))
-    pounds = kilograms / lbs_to_kg
-    print("The number of pounds is", pounds)
+num = eval(input('Enter a number you wish to convert from pounds to kg: '))
+
+# Evaluate mass in kg
+num_kg = num * lbs_to_kg
+
+# Print result
+print('{0} lbs is {1:0.3f} kg'.format(num, num_kg))
